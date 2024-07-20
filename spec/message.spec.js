925e82d79a2d6b10e774c176bc1e1e05458faad0
Stephen Bradshaw <root@Elliss-MacBook-Pro.local>
2024-07-20 15:23:15 -0400

    Mars Rover graded assignemnt. Command/Message classes

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -5,5 +5,21 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Message class", function() {
+    it("throws error if name is NOT passed into constructor as the first parameter", function() {
+        expect( function() { new Message();}).toThrow(new Error('Name required.'));
+    });
+
+    it("“constructor sets command type”", function() {
+        let name = 'NAME';
+        let checkName = new Message(name);
+        expect(checkName.name).toBe(name);
+    });
+    
+    it('contains a commands array passed into the constructor as the 2nd argument', function() {
+        let name = 'NAME';
+        let commands = ['MODE_CHANGE', 'LOW_POWER'];  
+        let message = new Message(name, commands);
+        expect(message.commands).toBe(commands);
+  });
 
 });
